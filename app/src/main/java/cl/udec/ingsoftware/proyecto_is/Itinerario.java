4ff22ed217c98e5d10356e46dd93d834a9f631ae
dirodriguezm <drodriguezm@udec.cl>
2016-12-28 21:28:55 -0300

    Implementación de Crear itinerario, Visualizar Itinerario

--- app/src/main/java/cl/udec/ingsoftware/proyecto_is/Itinerario.java
+++ app/src/main/java/cl/udec/ingsoftware/proyecto_is/Itinerario.java
@@ -5,4 +5,15 @@ package cl.udec.ingsoftware.proyecto_is;
  */
 
 public class Itinerario {
+    private String nombre;
+    public Itinerario(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
 }
