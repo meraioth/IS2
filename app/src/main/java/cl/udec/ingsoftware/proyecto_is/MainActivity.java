abf129be07fcdf8f52025073a0c9444bd877afac
Meraioth Ulloa <mb.us.94@gmail.com>
2016-12-28 22:18:09 -0300

    conexion con bd

--- app/src/main/java/cl/udec/ingsoftware/proyecto_is/MainActivity.java
+++ app/src/main/java/cl/udec/ingsoftware/proyecto_is/MainActivity.java
@@ -27,14 +27,15 @@ DBconnect bd;
                 // "jdbc:postgresql://IP:PUERTO/DB", "USER", "PASSWORD");
                 // Si estás utilizando el emulador de android y tenes el PostgreSQL en tu misma PC no utilizar 127.0.0.1 o localhost como IP, utilizar 10.0.2.2
                 Connection conn = DriverManager.getConnection(
-                        "jdbc:postgresql://plop.inf.udec.cl/Matias","matiasmedina", "Psmlgipxfq1");
+                        "jdbc:postgresql://plop.inf.udec.cl/Matias?currentSchema=is","matiasmedina", "Psmlgipxfq1");
                 System.out.println("entro");
                 //En el stsql se puede agregar cualquier consulta SQL deseada.
-                String stsql = "Select version()";
+                String stsql = "Select * from servicio;";
                 Statement st = conn.createStatement();
                 ResultSet rs = st.executeQuery(stsql);
                 rs.next();
-                System.out.println( rs.getString(1) );
+                System.out.println(rs.getString("nombre_servicio"));
+                //System.out.println( rs.getString(1) );
                 conn.close();
             } catch (SQLException se) {
                 System.out.println("oops! No se puede conectar. Error: " + se.toString());
