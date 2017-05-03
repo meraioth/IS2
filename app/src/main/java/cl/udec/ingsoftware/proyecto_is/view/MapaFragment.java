0713d1cfc3d09b89800df7f917a632658fd734f3
matisin <matias.medina.s2@gmail.com>
2017-05-03 16:02:10 -0300

    ultimo cambio

--- app/src/main/java/cl/udec/ingsoftware/proyecto_is/view/MapaFragment.java
+++ app/src/main/java/cl/udec/ingsoftware/proyecto_is/view/MapaFragment.java
@@ -1,6 +1,5 @@
 package cl.udec.ingsoftware.proyecto_is.view;
 
-import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,16 +7,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import cl.udec.ingsoftware.proyecto_is.R;
-import cl.udec.ingsoftware.proyecto_is.databinding.BusquedaFragmentBinding;
 
 
-public class BusquedaFragment extends Fragment {
+public class MapaFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        BusquedaFragmentBinding binding = DataBindingUtil.inflate(
-                inflater, R.layout.busqueda_fragment, container, false);
-        View view = binding.getRoot();
-        return view;
+        /**
+         * Inflate the layout for this fragment
+         */
+        return inflater.inflate(R.layout.fragment_mapa, container, false);
     }
 }
